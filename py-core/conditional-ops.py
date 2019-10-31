0b64092a051aedc9d340cd9dd0d4781563e85602
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-10-31 20:26:19 +0530

    Revert "Revert "logical operators""

---
+++ py-core/conditional-ops.py
@@ -0,0 +1,11 @@
+name = 'Ranjith'
+isMale = False
+isFemale = False
+
+if isMale:
+    print("Mr. " + name)
+elif isFemale:
+    print('Mrs. ' + name)
+else:
+    print("Dear " + name)
+print("End of program")
