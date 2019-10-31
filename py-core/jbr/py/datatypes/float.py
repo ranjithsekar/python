d978fa2df96f1dc817be6cbf1ad1690d4a86a1af
Ranjith Sekar <ranjith.sekar@clarivate.com>
2019-10-31 17:00:47 +0530

    Revert "update2"
    This reverts commit 3a22bc32

--- py-core/jbr/py/datatypes/float.py
+++
@@ -1,5 +0,0 @@
-# Float
-weight = input('Enter your weight (in kg): ')
-height = input('Enter your height (in cms): ')
-bmi = float(weight) / (int(height) / 100 * int(height) / 100)
-print('Your BMI value is: ' + str(bmi))
--- py-core/jbr/py/datatypes/integer.py
+++
@@ -1,4 +0,0 @@
-# Integer
-birth_year = input("Enter your birth year: ")
-age = 2019 - int(birth_year)
-print('Your Age is: ' + age)
